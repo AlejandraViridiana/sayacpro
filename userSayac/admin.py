8c38aee9780e0ba3701f5da960a24fae25d9e063
Gina Arlyn Gutierrez Mata <54778600+GinaArlyn@users.noreply.github.com>
2020-03-16 23:02:36 -0700

    Delete admin.py

--- userSayac/admin.py
+++
@@ -1,7 +0,0 @@
-from .models import User,Program,Academy
-from django.contrib import admin
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Program)
-admin.site.register(Academy)
