592f6eb933f2dec423e6aadd3dee5235a7757f90
pablo-alejos <pablo.alejos@uabc.edu.mx>
2020-03-24 20:53:07 -0700

    creda carpeta /img/icons8 y agregados iconos, agregado sap.css hoja de estilos

--- userSayac/urls.py
+++ userSayac/urls.py
@@ -12,14 +12,14 @@ urlpatterns = [
     path('academy/<int:id>/delete',AcademyDeleteView.as_view(),name='academy-delete'),
     path('academy/<int:id>/update',AcademyUpdateView.as_view(),name='academy-update'), 
 
-   #---------------- Path para Programas
+    #---------------- Path para Programas
     path('program/index',ProgramIndexView.as_view(),name='program-index'),
     path('program/create',ProgramCreateView.as_view(),name='program-create'), 
     path('program/<int:id>/',ProgramDetailView.as_view(),name='program-detail'),  
     path('program/<int:id>/delete',ProgramDeleteView.as_view(),name='program-delete'),
     path('program/<int:id>/update',ProgramUpdateView.as_view(),name='program-update'), 
 
- #---------------- Path para usuario
+    #---------------- Path para usuario
     path('user/index',UserIndexView.as_view(),name='user-index'),
     path('user/create',UserCreateView.as_view(),name='user-create'), 
     path('user/<int:id>/',UserDetailView.as_view(),name='user-detail'),  
